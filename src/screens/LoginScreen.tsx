c3c2940137182ba6e6cef7f4844dde03d57f3d13
Clovis <clovis.norroy@gmail.com>
2020-07-12 20:28:24 +0200

    Made the login screen

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,22 +1,50 @@
-import React from 'react';
-import { Text, StyleSheet, Button, View } from 'react-native';
-
+import React, { useState } from 'react';
+import { Text, StyleSheet, Button, View, TouchableOpacity } from 'react-native';
+import { Input } from 'react-native-elements';
+import ciasieChat from '../api/ciasieChat';
 
 function LoginScreen({navigation}: any){
+    const [isLogin, setIsLogin] = useState(false);
+    const [login, setLogin] = useState("");
+    const [password, setPassword] = useState("");
+    const [repeatPassword, setRepeatPassword] = useState("");
     return (
-        <View>
-    <Text style={styles.textStyle}> Login Screen</Text>
-    <Button
-    title="Go to Messages"
-    onPress={() => navigation.navigate('Messages')}
-  />
-  </View>
+        <View style={styles.marginTop}>
+            <Input placeholder="Email" value={login}/>
+            <Input placeholder="Password" value={password}/>
+            {isLogin && <Input placeholder="Repeat Password"/>}
+            <TouchableOpacity 
+                onPress={() => {
+                    setIsLogin(false)
+                }}
+                style={styles.buttonStyle}>
+                <Text>Login</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => {
+                setIsLogin(true)
+            }} style={styles.buttonStyle}>
+                <Text>Create Account</Text>
+            </TouchableOpacity>
+        </View>
     );
 };
+
+function Register(){
+    
+}
 
 const styles = StyleSheet.create({
     textStyle: {
         fontSize: 30
+    },
+    marginTop: {
+        marginTop: 25
+    },
+    buttonStyle: {
+        margin: 15,
+        alignItems: "center",
+        backgroundColor: "#DDDDDD",
+        padding: 10
     }
 })
 
